ce986701689bc80cb7228a60c07b444bb1d82a9d
zakaria-69 <ladjrafi_zakaria@yahoo.com>
2023-02-08 14:52:45 +0100

    add presentation section + profilPicture

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Presentation = () => {
     return (
         <main className='presentation__container presentation__anim'>
-            <h1>Bonjour,je suis Ladjrafi Zakaria</h1>
+            <h1>Bienvenue <br/>je suis Ladjrafi Zakaria</h1>
             <section className='presentation__container__animation'>
                 <div className='first'>
                     <div>
@@ -12,7 +12,7 @@ const Presentation = () => {
                 </div>
                 <div className='second'>
                     <div>
-                        Développeur Integrateur web
+                        Développeur web
                     </div>
                 </div>
                 <div className='third'>
@@ -46,6 +46,34 @@ const Presentation = () => {
                     </div>
                 </div>
             </section>
+            <div className='presentation__container__description'>
+                <div className='presentation__container__description__left'> 
+                    <p className='presentation__container__description__left__textContent'>
+                        Depuis très jeune j'ai une appétence pour le monde informatique,le digital,
+                        la technologie.En cherchant à savoir plus en détail comment étaient crée les site internet,
+                        les jeux-vidéos et plus récemment les applications,comment intérragisseaient entre eux les éléments  
+                        et les appareils pour obtenir le résultat éscompté,c'est de cette réflexion que m'est venu l'envie d'apprendre 
+                        des langages informatique.Après quelques projets personnel et ayant vu le pouvoir de ces outils,j'ai
+                        vu mon intérêt et mon envie d'en apprendre encore plus décuplée.<br/>
+                        J'ai donc suivi cette voie sur un cursur professionnalisant en titre de  
+                        <span className='presentation__container__description__left__textContent__profession'> développeur intégrateur Web </span> 
+                         au cours de l'année 2022 et j'aspire à pouvoir
+                        continuer d'apprendre et surtout d'éxercer car c'est selon moi le meilleurs moyen de s'améliorer.
+                    </p>
+                    </div>
+                    <div className='presentation__container__description__right'>
+                        <div className='presentation__container__description__right__img'>
+                        <img src='../../../img/profil-cv.jpg'></img>
+                        </div>
+                        <div>
+                        <p className='presentation__container__description__right__ofGripSentence'>
+                        La richesse se trouve dans la connaissance,chacun donc à son trésor à partager.
+                    </p>
+                        </div>
+                    
+                    </div>
+               
+            </div>
         </main>
     );
 }
